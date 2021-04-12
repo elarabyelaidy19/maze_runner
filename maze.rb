7ec4c34acacd6ae49bbf7b1166ea9ed59f954f74
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-12 11:36:06 +0200

    lost

--- maze.rb
+++ maze.rb
@@ -1,4 +1,19 @@
 module MazeClasses 
 
-  class Maze 
-    
+  class Maze  
+    DELTAS = [[1, 0], [0, 1], [-1, 0], [0, -1]] 
+
+    attr_reader :start_inedx, :end_inedx 
+
+    def initialize(filename) 
+      @map = load_map(filename) 
+
+    end 
+
+    def load_map(filename) 
+      maze = [] 
+      File.open(filename).each_line do |line| 
+        chars = line.split(//) 
+        
+
+
